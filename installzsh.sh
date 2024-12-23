d66cb551d858b90069ac9a8f2c2f51c8aa4e2a12
Begitdj <egyptha29@gmail.com>
2024-12-23 19:14:58 +0700

    Create installzsh.sh

---
+++ installzsh.sh
@@ -0,0 +1,4 @@
+echo install zsh and curl and termux-api
+apt install zsh curl termux-api -y
+echo install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
