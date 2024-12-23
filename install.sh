92416da9d8544d5e55dacda84eb97f07233ae240
Begitdj <egyptha29@gmail.com>
2024-12-23 15:14:25 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,16 @@
-echo Installing...
-apt install zsh git -y
+echo Hello this is install script for my termux dotfile
+echo install zsh and curl
+apt install zsh curl -y
+echo install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo install powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+rm -rf ~/.zshrc
+echo "ZSH_THEME="powerlevel10k/powerlevel10k" >> ~/.zshrc
+echo install lsd and change ls to lsd
 apt install lsd
 echo "alias ls=lsd" >> ~/.zshrc
+echo install fonts colors and p10k dotfile
 mkdir $HOME/.termux
 cp ./colors.properties $HOME/.termux/colors.properties
 cp ./font.ttf $HOME/.termux/font.ttf
