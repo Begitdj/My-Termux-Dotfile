b767ec7ae2ade26644c72ef44e631dcd7eb56143
Begitdj <egyptha29@gmail.com>
2024-12-23 22:18:37 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,18 @@
+echo Hello this is install script for my termux dotfile
+echo install powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+rm -rf ~/.zshrc
+cp ./.zshrc ~/.zshrc
+echo install lsd
+apt install lsd
+echo install fonts colors extra key and p10k dotfile
+rm -rf ~/.termux/termux.properties
+cp ./termux.properties ~/.termux/termux.properties
+rm -rf ~/.termux/color.properties
+cp ./colors.properties ~/.termux/colors.properties
+rm -rf ~/.termux/font.ttf
+cp ./font.ttf ~/.termux/font.ttf
+rm -rf ~/.p10k.zsh
+cp ./.p10k.zsh ~/.p10k.zsh
+termux-reload-settings
+echo Install Complete! Full restart termux to change shell theme
