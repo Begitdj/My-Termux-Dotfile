4e9873b29f03aa1044846b1ee0a0d1d8db220f86
Begitdj <egyptha29@gmail.com>
2024-12-23 18:08:28 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 echo install powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 rm -rf ~/.zshrc
-cp ./.zshrc.zsh ~/.zshrc
+cp ./zshrc.zsh ~/.zshrc
 echo install lsd
 apt install lsd
 echo install fonts colors and p10k dotfile
