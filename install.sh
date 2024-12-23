e1f646320dd55b2249a37fb248faef269a0e00ae
Begitdj <egyptha29@gmail.com>
2024-12-23 18:07:26 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,13 +6,12 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 echo install powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 rm -rf ~/.zshrc
-echo "ZSH_THEME="powerlevel10k/powerlevel10k" >> ~/.zshrc
-echo install lsd and change ls to lsd
+cp ./.zshrc.zsh ~/.zshrc
+echo install lsd
 apt install lsd
-echo "alias ls=lsd" >> ~/.zshrc
 echo install fonts colors and p10k dotfile
-mkdir $HOME/.termux
-cp ./colors.properties $HOME/.termux/colors.properties
-cp ./font.ttf $HOME/.termux/font.ttf
-cp ./.p10k.zsh $HOME/.p10k.zsh
+mkdir ~/.termux
+cp ./colors.properties ~/.termux/colors.properties
+cp ./font.ttf ~/.termux/font.ttf
+cp ./.p10k.zsh ~/.p10k.zsh
 echo Install Complete!
